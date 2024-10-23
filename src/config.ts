e507e72c74a1a0ca0b746d589b642584f789b72d
geovgy <gbh218@gmail.com>
2024-10-23 16:07:55 -0400

    Resolve issue #48: update voting period

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const ROUND = 6;
 export const agoraRoundsAPI = `/api/agora/retrofunding/rounds/${ROUND}`;
 
 // needs to be adjusted when we get the correct date
-export const votingEndDate = new Date(Date.UTC(2024, 10, 5, 18, 0, 0));
+export const votingEndDate = new Date(Date.UTC(2024, 10, 11, 18, 0, 0));
 
 export const badgeholderManualUrl =
   'https://gov.optimism.io/t/retro-funding-6-badgeholder-manual/9109';
